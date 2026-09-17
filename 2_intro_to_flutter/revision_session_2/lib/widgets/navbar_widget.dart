7ba1d1c0f99c640520675e2d5ef1a8e6340ee797
waseem-development <hafizwaseemahmed2002@gmail.com>
2026-09-17 08:12:51 +0500

    About to learn further stuff

---
+++ 2_intro_to_flutter/revision_session_2/lib/widgets/navbar_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/widgets.dart';
+
+class NavbarWidget extends StatefulWidget {
+  const NavbarWidget({super.key});
+
+  @override
+  State<NavbarWidget> createState() => _NavbarWidgetState();
+}
+
+class _NavbarWidgetState extends State<NavbarWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
